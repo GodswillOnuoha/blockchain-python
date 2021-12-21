45e721143e15a82f0d7b6be74159711a3ad3989e
Godswill <godswill@carlabs.com>
2021-12-21 06:06:09 +0100

    add nodes to the blockchain

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
+from argparse import ArgumentParser
+
 from blockchain import create_node
 
+parser = ArgumentParser()
+parser.add_argument('-p', '--port', default=3000)
+
+args = parser.parse_args()
+
+
 node = create_node()
-node.start()
+node.start(port=args.port)
